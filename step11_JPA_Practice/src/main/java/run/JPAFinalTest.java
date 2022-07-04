c055588b2fe6a29b0377cb87159141865dd5aacd
Uno0306 <82020768+Uno0306@users.noreply.github.com>
2022-07-04 18:03:37 +0900

    step11.change to id, Auto increment

--- step11_JPA_Practice/src/main/java/run/JPAFinalTest.java
+++ step11_JPA_Practice/src/main/java/run/JPAFinalTest.java
@@ -20,7 +20,7 @@ public class JPAFinalTest {
 //			JPA02CRUD.insert(em, 2, "Ali");
 //			JPA02CRUD.insert(em, 30, "Paster");
 //			JPA02CRUD.insert(em, 60, "Endy");
-			JPA02CRUD.insert(em, 25, "Wendy");
+//			JPA02CRUD.insert(em, 25, "Wendy");
 			
 //			JPA02CRUD.selectById(em, 13245646L);
 			
@@ -30,7 +30,7 @@ public class JPAFinalTest {
 //			JPA02CRUD.delete(em, 13245646L);
 			
 //			JPA02CRUD.selectAll(em);
-//			JPA02CRUD.selectAllName(em);
+			JPA02CRUD.selectAllName(em);
 			
 			tx.commit();
 		} catch (Exception e) {
